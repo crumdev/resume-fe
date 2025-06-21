563744c1d53c5c1c07f0bd1a7f68a29d2b0b5b54
Nathan Crum <crumdev@users.noreply.github.com>
2025-06-21 02:01:06 +0000

    fixed terraform formatting and added fmt diff output to ci

--- infrastructure/cloudflare.tf
+++ infrastructure/cloudflare.tf
@@ -13,5 +13,5 @@ resource "cloudflare_dns_record" "cname_azure_static_resume_fe" {
   comment = "Cname to Azure Static Resume Frontend"
   content = azurerm_storage_account.sa-eastus-resume-fe.primary_web_host
   proxied = true
-  ttl = 1
+  ttl     = 1
 }

cc765f61c36ec4e3875b859a8209ccc7c1bca92d
Nathan Crum <crumdev@users.noreply.github.com>
2025-07-17 03:16:20 +0000

    correcting dns settings

--- infrastructure/cloudflare.tf
+++ infrastructure/cloudflare.tf
@@ -8,10 +8,10 @@ resource "cloudflare_zone" "crumdev" {
 
 resource "cloudflare_dns_record" "cname_azure_static_resume_fe" {
   zone_id = cloudflare_zone.crumdev.id
-  name    = "@"
+  name    = "@" # or "www" if you want www.crumdev.com
   type    = "CNAME"
-  comment = "Cname to Azure Static Resume Frontend"
-  content = azurerm_storage_account.sa-eastus-resume-fe.primary_web_host
+  comment = "CNAME to Azure CDN for Resume Frontend"
+  content = azurerm_cdn_endpoint.resume_fe_cdn_endpoint.host_name
   proxied = true
   ttl     = 1
 }
